da6d3e5c11753fa6dd5305e6b90facad7c91f560
Evgeniy <39721548+mrHamster86@users.noreply.github.com>
2018-11-23 10:55:35 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,4 +1,4 @@
-import {INITIAL_GAME} from '../game.js';
+import {INITIAL_GAME} from './game.js';
 
 const LEVELS = INITIAL_GAME.levels;
 const BONUS_TIME = 30;
@@ -6,7 +6,7 @@ const LOSING_SCORE = -1;
 const INCORRECT_POINTS = -2;
 
 const correctPoints = (time, bonusTime) => {
-  return (time >= bonusTime) ? 1 : 2;
+  return (time >= BONUS_TIME) ? 1 : 2;
 };
 
 const calculateScore = (answers) => {
@@ -14,7 +14,7 @@ const calculateScore = (answers) => {
   let result = 0;
 
   for (const it of answers) {
-    result += (!it.correct) ? INCORRECT_POINTS : correctPoints(it.time, BONUS_TIME);
+    result += (!it.correct) ? INCORRECT_POINTS : correctPoints(it.time);
     lives += (!it.correct) ? -1 : 0;
     if (lives < 0) break;
   }
