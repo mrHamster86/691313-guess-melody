84144222afb5f948ff12a8417c35c6dcce5f783c
Evgeniy <39721548+mrHamster86@users.noreply.github.com>
2018-11-23 08:54:06 +0000

    Update game.test.js

--- js/test/game.test.js
+++ js/test/game.test.js
@@ -1,5 +1,5 @@
 import {assert} from 'chai';
-import {changeLevel, changeLives, changeTime} from '../gameState';
+import {INITIAL_GAME, changeLevel, changeLives, changeTime} from '../gameState';
 
 describe(`Проверка смены уровня`, () => {
   it(`Проверка изменения уровня`, () => {
@@ -20,32 +20,32 @@ describe(`Проверка смены уровня`, () => {
 
 describe(`Управление колличеством жизней игрока`, () => {
   it(`Проверка изменения колличества жизней`, () => {
-    assert.equal(changeLevel(INITIAL_GAME, 1).lives, 1);
-    assert.equal(changeLevel(INITIAL_GAME, 0).lives, 0);
+    assert.equal(changeLives(INITIAL_GAME, 1).lives, 1);
+    assert.equal(changeLives(INITIAL_GAME, 0).lives, 0);
   });
   it(`Количество жизней не может иметь отрицательное значение`, () => {
-    assert.throws(() => changeLevel(INITIAL_GAME, -1).lives, 0);
+    assert.throws(() => changeLives(INITIAL_GAME, -1).lives, 0);
   });
   it(`Количество жизней не может иметь отрицательное значение`, () => {
-    assert.throws(() => changeLevel(INITIAL_GAME, []).lives, 0);
+    assert.throws(() => changeLives(INITIAL_GAME, []).lives, 0);
   });
   it(`Количество жизней не может иметь значение больще значения в начале`, () => {
-    assert.throws(() => changeLevel(INITIAL_GAME, 11).lives, 0);
+    assert.throws(() => changeLives(INITIAL_GAME, 11).lives, 0);
   });
 });
 
 describe(`Управление таймером`, () => {
   it(`Проверка изменения колличества секунд`, () => {
-    assert.equal(changeLevel(INITIAL_GAME, 100).time, 1);
-    assert.equal(changeLevel(INITIAL_GAME, 0).time, 0);
+    assert.equal(changeTime(INITIAL_GAME, 100).time, 1);
+    assert.equal(changeTime(INITIAL_GAME, 0).time, 0);
   });
   it(`Таймер не может иметь отрицательное значение`, () => {
-    assert.throws(() => changeLevel(INITIAL_GAME, -1).time, 0);
+    assert.throws(() => changeTime(INITIAL_GAME, -1).time, 0);
   });
   it(`Таймер не может иметь отрицательное значение`, () => {
-    assert.throws(() => changeLevel(INITIAL_GAME, []).time, 0);
+    assert.throws(() => changeTime(INITIAL_GAME, []).time, 0);
   });
   it(`Таймер не может иметь значение больще значения в начале`, () => {
-    assert.throws(() => changeLevel(INITIAL_GAME, 11).time, 0);
+    assert.throws(() => changeTime(INITIAL_GAME, 11).time, 0);
   });
 });
