91396be6e7593c2ea84ba0f3b8d0ceb1497d2796
mrHamster86 <39721548+mrHamster86@users.noreply.github.com>
2018-12-01 17:56:18 +0300

    #8 И ехать и шашечки! - 3

--- js/animation/animation.js
+++ js/animation/animation.js
@@ -1,8 +1,5 @@
 import {INITIAL_GAME} from '../data/data.js';
 
-const RADIUS = 370;
-const TIMER_LINE = document.querySelector(`.timer__line`);
-
 const getCircumference = (radius) => Math.round(2 * Math.PI * radius);
 
 export const getRadius = (ratioTime, radius) => {
@@ -15,9 +12,13 @@ export const getRadius = (ratioTime, radius) => {
 };
 
 export const getDash = (time) => {
+  const RADIUS = 370;
+  const TIMER_LINE = document.querySelector(`.timer__line`);
+
   const ratioTime = time / INITIAL_GAME.time;
   const dashState = getRadius(ratioTime, RADIUS);
 
   TIMER_LINE.setAtribute(`stroke-dasharray`, dashState.stroke);
   TIMER_LINE.setAtribute(`stroke-dashoffset`, dashState.offset);
 };
+
