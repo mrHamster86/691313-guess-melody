9311f19c5ea942982e457a421adabca5614260cd
mrHamster86 <39721548+mrHamster86@users.noreply.github.com>
2018-12-18 22:48:35 +0300

    #12 Бессердечная статистика - на защиту

--- js/test/result.test.js
+++ js/test/result.test.js
@@ -1,15 +1,21 @@
 import {assert} from 'chai';
 import {getGameResult} from '../data/game-result';
 
-const plaerResult1 = {score: 10, lives: 1, time: 10};
+const plaerResult1 = {score: 19, lives: 1, time: 10};
 const plaerResult2 = {score: -1, lives: 1, time: 0};
 const plaerResult3 = {score: -1, lives: 0, time: 40};
 
-const statistics = [4, 5, 8, 10, 11];
+const statistics = [
+  {score: 12},
+  {score: 20},
+  {score: 19},
+  {score: 10},
+  {score: 18}
+];
 
 describe(`Функция вывода результата игрока`, () => {
   it(`Если игрок выиграл`, () => {
-    assert.equal(getGameResult(plaerResult1, statistics).content, `Вы заняли 3 место из 6 игроков. Это лучше, чем у 50% игроков`);
+    assert.equal(getGameResult(plaerResult1, statistics).content, `Вы заняли 2 место из 5 игроков. Это лучше, чем у 60% игроков`);
   });
   it(`Если время истекло`, () => {
     assert.equal(getGameResult(plaerResult2, statistics).content, `Время вышло! Вы не успели отгадать все мелодии`);
